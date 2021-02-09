95e5ad92896d56efeb14a3ede2b4d761b882228f
Max <Max@bizzwc.local>
2021-02-10 03:55:51 +0500

    working on group chat and pusher

--- src/components/users-window/create-group-modal/SelectGroupMembers.js
+++ src/components/users-window/create-group-modal/SelectGroupMembers.js
@@ -1,5 +1,32 @@
 import React, { Component } from "react";
+import Scrollbars from "react-custom-scrollbars";
+import ChatThumb from "../chat-thumb/ChatThumb";
+
+
 class SelectGroupMembers extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+        };
+    }
+    componentDidMount() {
+        let user = JSON.parse(localStorage.getItem('user'))
+        if (!user) {
+            this.props.history.push('/login')
+        }
+        else {
+            axios.post(`http://192.168.0.96:401/bwccrm/getContactsTotal`, { user_id: user.data.user_id, campaign_id: 1 })
+                .then(res => {
+                    // console.log(res.data.contacts)
+                    this.setState({ data: res.data.contacts })
+                })
+                .catch((err) => {
+                    console.log(err.message)
+                })
+        }
+
+    }
     render() {
         if (this.props.currentStep !== 2) {
             return null
@@ -7,16 +34,17 @@ class SelectGroupMembers extends React.Component {
 
         return (
             <div className="form-group">
-                <label htmlFor="email">phone</label>
-                <input
-                    className="form-control"
-                    id="email"
-                    name="email"
-                    type="text"
-                    placeholder="Enter email"
-                    value={this.props.email} 
-                    onChange={this.props.handleChange} 
-                />
+                <div className="recent-chats">
+                    <Scrollbars autoHide>
+                        {data.map((e, i) => {
+                            return (
+                                <div onClick={() => this.showChat(e.user_id)} key={i}>
+                                    <ChatThumb data={e} />
+                                </div>
+                            );
+                        })}
+                    </Scrollbars>
+                </div>
             </div>
         )
     }
